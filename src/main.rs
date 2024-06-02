850dac1485fd9aaa08719db9e883e696e2784ff4
igorlnunes <igorlnunes@gmail.com>
2024-06-01 21:09:04 -0300

    feat: second screen

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,8 @@ use ethers::{
 use eyre::Result;
 use std::convert::TryFrom;
 use std::sync::Arc;
+use std::time::{Duration, Instant};
 use tokio;
-
-// todo
-/*  > Handle errors;
-   Ok Handle 0 coins in wallet;
-   Ok Handle wrong address pattern - if user input a contract address return - "This is not a valid wallet address"
-   > Multichain;
-   Ok User can be able to change the chain(chainId) for those: Ethereum, Polygon, Optimism
-   > UX CLI:
-   Ok pretty formatting;
-   > change output print;
-   > Using up-to-date libs;
-   - This is needed because ethers.rs has been deprecated
-
-*/
 
 #[derive(Debug)]
 enum Network {
@@ -57,6 +44,7 @@ struct App<'a> {
     provider: Option<Provider<Http>>,
     state: AppState,
     selected_network: Option<Network>,
+    balance_output: String, // Novo campo para armazenar o saldo formatado
 }
 
 impl<'a> App<'a> {
@@ -68,6 +56,7 @@ impl<'a> App<'a> {
             provider: None,
             state: AppState::SelectNetwork,
             selected_network: None,
+            balance_output: String::new(), // Inicializando o novo campo
         }
     }
 
@@ -86,6 +75,17 @@ impl<'a> App<'a> {
 
 #[allow(non_snake_case)]
 async fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App<'_>) -> Result<()> {
+    let tick_rate = Duration::from_millis(250);
+    let mut last_tick = Instant::now();
+
+    let messages = vec![
+        "Insira sua carteira Ethereum...",
+        "Insira sua carteira Ethereum.",
+        "Insira sua carteira Ethereum..",
+        "Insira sua carteira Ethereum...",
+    ];
+    let mut idx = 0;
+
     loop {
         terminal.draw(|f| {
             let size = f.size();
@@ -137,18 +137,38 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App<'_>) -> R
                         .title("Digite o endereço da wallet:")
                         .borders(Borders::ALL);
 
-                    let paragraph = Paragraph::new(app.address_input.as_ref())
+                    let chunks = Layout::default()
+                        .direction(Direction::Vertical)
+                        .constraints(
+                            [
+                                Constraint::Percentage(45),
+                                Constraint::Percentage(10),
+                                Constraint::Percentage(45),
+                            ]
+                            .as_ref(),
+                        )
+                        .split(size);
+
+                    let input = Spans::from(vec![Span::raw(app.address_input.as_str())]);
+
+                    let message = vec![Spans::from(Span::raw(messages[idx]))];
+
+                    let input_paragraph =
+                        Paragraph::new(input).block(Block::default().borders(Borders::NONE));
+                    f.render_widget(input_paragraph, chunks[1]);
+
+                    let paragraph = Paragraph::new(message)
                         .block(block)
                         .alignment(Alignment::Left);
 
-                    f.render_widget(paragraph, size);
+                    f.render_widget(paragraph, chunks[0]);
                 }
                 AppState::ShowBalances => {
                     let block = Block::default()
                         .title("Saldo da Wallet")
                         .borders(Borders::ALL);
 
-                    let paragraph = Paragraph::new(app.address_input.as_ref())
+                    let paragraph = Paragraph::new(app.balance_output.as_str())
                         .block(block)
                         .alignment(Alignment::Left);
 
@@ -157,108 +177,117 @@ async fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App<'_>) -> R
             }
         })?;
 
-        if let Event::Key(key) = event::read()? {
-            match app.state {
-                AppState::SelectNetwork => match key.code {
-                    KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Down => app.next(),
-                    KeyCode::Up => app.previous(),
-                    KeyCode::Char('e') => {
-                        app.selected_network = Some(match app.selected {
-                            0 => Network::Ethereum(
-                                1,
-                                "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-                                    .parse::<Address>()
-                                    .unwrap(),
-                            ),
-                            1 => Network::Polygon(
-                                137,
-                                "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
-                                    .parse::<Address>()
-                                    .unwrap(),
-                            ),
-                            2 => Network::Optimism(
-                                10,
-                                "0x0b2c639c533813f4aa9d7837caf62653d097ff85"
-                                    .parse::<Address>()
-                                    .unwrap(),
-                            ),
-                            _ => unreachable!(),
-                        });
-                        app.state = AppState::EnterAddress;
-                    }
-                    _ => {}
-                },
-                AppState::EnterAddress => match key.code {
-                    KeyCode::Char(c) => {
-                        app.address_input.push(c);
-                    }
-                    KeyCode::Backspace => {
-                        app.address_input.pop();
-                    }
-                    KeyCode::Enter => {
-                        if let Some(selected_network) = &app.selected_network {
-                            let (chain, usdc_contract) = match selected_network {
-                                Network::Ethereum(chain_id, address) => (chain_id, address),
-                                Network::Polygon(chain_id, address) => (chain_id, address),
-                                Network::Optimism(chain_id, address) => (chain_id, address),
-                            };
+        if crossterm::event::poll(tick_rate)? {
+            if let Event::Key(key) = event::read()? {
+                match app.state {
+                    AppState::SelectNetwork => match key.code {
+                        KeyCode::Char('q') => return Ok(()),
+                        KeyCode::Down => app.next(),
+                        KeyCode::Up => app.previous(),
+                        KeyCode::Char('e') => {
+                            app.selected_network = Some(match app.selected {
+                                0 => Network::Ethereum(
+                                    1,
+                                    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+                                        .parse::<Address>()
+                                        .unwrap(),
+                                ),
+                                1 => Network::Polygon(
+                                    137,
+                                    "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"
+                                        .parse::<Address>()
+                                        .unwrap(),
+                                ),
+                                2 => Network::Optimism(
+                                    10,
+                                    "0x0b2c639c533813f4aa9d7837caf62653d097ff85"
+                                        .parse::<Address>()
+                                        .unwrap(),
+                                ),
+                                _ => unreachable!(),
+                            });
+                            app.state = AppState::EnterAddress;
+                        }
+                        _ => {}
+                    },
+                    AppState::EnterAddress => match key.code {
+                        KeyCode::Char(c) => {
+                            app.address_input.push(c);
+                        }
+                        KeyCode::Backspace => {
+                            app.address_input.pop();
+                        }
+                        KeyCode::Enter => {
+                            if let Some(selected_network) = &app.selected_network {
+                                let (chain, usdc_contract) = match selected_network {
+                                    Network::Ethereum(chain_id, address) => (chain_id, address),
+                                    Network::Polygon(chain_id, address) => (chain_id, address),
+                                    Network::Optimism(chain_id, address) => (chain_id, address),
+                                };
 
-                            let url_chain: &str;
+                                let url_chain: &str;
 
-                            if *chain == 1 {
-                                url_chain = "mainnet"
-                            } else if *chain == 137 {
-                                url_chain = "polygon-mainnet"
-                            } else if *chain == 10 {
-                                url_chain = "optimism-mainnet"
-                            } else {
-                                url_chain = "err"
-                            }
+                                if *chain == 1 {
+                                    url_chain = "mainnet"
+                                } else if *chain == 137 {
+                                    url_chain = "polygon-mainnet"
+                                } else if *chain == 10 {
+                                    url_chain = "optimism-mainnet"
+                                } else {
+                                    url_chain = "err"
+                                }
 
-                            // Check if .env file is ok - get API_KEY
+                                // Check if .env file is ok - get API_KEY
 
-                            dotenvy::dotenv().ok();
-                            let INFURA_API_KEY: String =
-                                dotenvy::var("INFURA_API_KEY").expect("API KEY is needed");
+                                dotenvy::dotenv().ok();
+                                let INFURA_API_KEY: String =
+                                    dotenvy::var("INFURA_API_KEY").expect("API KEY is needed");
 
-                            // Instantiate the provider given a blockchain connection
-                            let provider: Provider<Http> = Provider::try_from(format!(
-                                "https://{}.infura.io/v3/{}",
-                                url_chain, INFURA_API_KEY
-                            ))
-                            .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-
-                            app.provider = Some(provider);
-
-                            let address_from: Address = app
-                                .address_input
-                                .trim()
-                                .parse::<Address>()
+                                // Instantiate the provider given a blockchain connection
+                                let provider: Provider<Http> = Provider::try_from(format!(
+                                    "https://{}.infura.io/v3/{}",
+                                    url_chain, INFURA_API_KEY
+                                ))
                                 .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
 
-                            if let Some(ref provider) = app.provider {
-                                print_balances(provider, address_from, *usdc_contract).await?;
-                            }
+                                app.provider = Some(provider);
 
-                            app.state = AppState::ShowBalances;
+                                let address_from: Address = app
+                                    .address_input
+                                    .trim()
+                                    .parse::<Address>()
+                                    .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+
+                                if let Some(ref provider) = app.provider {
+                                    app.balance_output =
+                                        print_balances(provider, address_from, *usdc_contract)
+                                            .await?;
+                                }
+
+                                app.state = AppState::ShowBalances;
+                            }
                         }
-                    }
-                    KeyCode::Esc => {
-                        app.state = AppState::SelectNetwork;
-                        app.address_input.clear();
-                    }
-                    _ => {}
-                },
-                AppState::ShowBalances => match key.code {
-                    KeyCode::Char('q') => return Ok(()),
-                    KeyCode::Esc => {
-                        app.state = AppState::SelectNetwork;
-                        app.address_input.clear();
-                    }
-                    _ => {}
-                },
+                        KeyCode::Esc => {
+                            app.state = AppState::SelectNetwork;
+                            app.address_input.clear();
+                        }
+                        _ => {}
+                    },
+                    AppState::ShowBalances => match key.code {
+                        KeyCode::Char('q') => return Ok(()),
+                        KeyCode::Esc => {
+                            app.state = AppState::SelectNetwork;
+                            app.address_input.clear();
+                        }
+                        _ => {}
+                    },
+                }
             }
+        }
+
+        if last_tick.elapsed() >= tick_rate {
+            last_tick = Instant::now();
+            idx = (idx + 1) % messages.len();
         }
     }
 }
@@ -267,7 +296,7 @@ async fn print_balances(
     provider: &Provider<Http>,
     address_from: Address,
     contract_usdc: Address,
-) -> eyre::Result<()> {
+) -> eyre::Result<String> {
     // Instantiate the ABI with an ERC20 interface
     abigen!(
         IERC20,
@@ -287,8 +316,7 @@ async fn print_balances(
         .map_err(|e| eyre::eyre!("Failed to get balance: {}", e))?;
 
     if result.is_zero() {
-        println!("The wallet has 0 USDC.");
-        return Ok(());
+        return Ok("The wallet has 0 USDC.".to_string());
     }
 
     let decimal: u8 = (contract.decimals().call())
@@ -299,12 +327,11 @@ async fn print_balances(
         .map_err(|e| eyre::eyre!("Failed to format units: {}", e))?;
 
     let output = format!(
-        "| Your wallet: {:#?} \n| has been: \n|          = {} USDC ",
+        "| Your wallet: {:#?} \n| has been: \n|                       = {} USDC ",
         address_from, formatted_result
     );
-    println!("{}", output);
 
-    Ok(())
+    Ok(output)
 }
 
 #[tokio::main]
